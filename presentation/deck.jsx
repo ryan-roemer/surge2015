474db108ea0d1a5e36797f2717ba64e299a13d9d
Ryan Roemer <ryan@loose-bits.com>
2015-08-18 23:40:34 -0700

    Plan more outline

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -99,7 +99,28 @@ export default class extends React.Component {
         {/*eslint-disable no-multiple-empty-lines*/
          /* TODO HERE
 
+            - OVERALL THEME - Wild West, "Large"
+
             - Dive into each of the bullet points above.
+                - Larger Apps
+                    - We want fast, responsive apps.
+                    - Have enormous possibilities: e.g., React with serevr-side render, bootstrap, into SPA
+                    - ... but the code bases are getting much, much larger.
+                - Larger Teams
+                    - With varying levels of experience.
+                        - HTML / CSS dev's learning large scale applications
+                        - Backend dev's learning the frontend.
+                - Harder to build / deploy / maintain / monitor
+                    - JS is the Wild, Wild West [image: wild west, cowboys]
+                    - Frameworks change every week
+                    - Libraries are of varying quality
+                    - We now have "The Build" for JS because it's that complicated.
+                        - Optimization for JS / web applications is complicated.
+                    - Code goes out in the wild, goes wrong and is really, really hard to support.
+                - Critical
+                    - The future is JS is on the critical path for apps to work.
+                    - JS errors are 5 or 6 figure issues.
+
             - INTRO: Introduction to Ryan / Formidable
                 - Help teams from startups to Fortune 500 companies.
                 - Talk about experiences with top 5 e-commerce website redesign.
@@ -107,9 +128,15 @@ export default class extends React.Component {
                 - Enormous code base.
                 - Widespread reach and traffic.
                 - Enormous array of supported browsers.
-            - THE POINT: Hone this down.
-            - ROADMAP / OUTLINE: Hone this down.
 
+            - THE POINT: Hone this down.
+
+            - ROADMAP / OUTLINE: Hone this down.
+                - TODO: Extract each of the bullet points from abstract on website.
+
+            - Failures & Lessons: DO SECTION
+
+            - The Future: DO SECTION
 
 
 
